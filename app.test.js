954494e11afdef4db8099b649b35f200c2d91754
Gansukh Batjargal <gansukhb@Gansukhs-MacBook-Pro.local>
2022-12-05 10:43:26 +0800

    Test with mongodb

--- app.test.js
+++ app.test.js
@@ -5,17 +5,37 @@ const mongoose = require("mongoose");
 beforeAll(async () => {
   if (!process.env.MONGODB_URL) {
     process.env.MONGODB_URL = "mongodb://localhost:27017/leap3_test";
-    mongoose.connect(process.env.MONGODB_URL);
+  }
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(process.env.MONGODB_URL);
     const connection = mongoose.connection;
 
     connection.once("open", () => {
-      console.log("Connected");
+      console.log("Connected to test server");
     });
   }
+});
+afterAll(async () => {
+  await mongoose.connection.close();
 });
 
 test("Test root path", async () => {
   const res = await request(app).get("/");
   expect(res.statusCode).toBe(200);
   expect(res.text).toBe("Hello World!");
+});
+
+test("Test user registrations", async () => {
+  const res = await request(app).post("/profile").send({
+    username: "test",
+    password: "test",
+    name: "test",
+    email: "email@email.com",
+    phone: "11223344",
+    gender: "m",
+    location: "Mongolia",
+  });
+  expect(res.statusCode).toBe(200);
+  const resData = JSON.parse(res.text);
+  expect(resData.message).toBe("Saved");
 });
